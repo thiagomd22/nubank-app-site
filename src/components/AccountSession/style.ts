59859120084a0c7f062951b14da45e64a0358925
Thiago Meireles <thiago22md@gmail.com>
2023-12-04 10:53:56 -0300

    update_style

--- src/components/AccountSession/style.ts
+++ src/components/AccountSession/style.ts
@@ -70,7 +70,7 @@ export const AnnounceCard = styled.div`
   background-color: ${theme.purpleGray};
 
   p {
-    font-size: 0.75rem;
+    font-size: 0.95rem;
     color: ${theme.black};
     line-height: 24px;
   }
